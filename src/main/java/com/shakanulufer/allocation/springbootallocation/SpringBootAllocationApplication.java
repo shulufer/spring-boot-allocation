aec0e7827a3b316bcfab64babd4d1b47c19af4b7
SalihHakanUlufer <hakanulufer@hotmail.com>
2023-10-27 14:29:29 +0200

    project was created with spring

---
+++ src/main/java/com/shakanulufer/allocation/springbootallocation/SpringBootAllocationApplication.java
@@ -0,0 +1,13 @@
+package com.shakanulufer.allocation.springbootallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAllocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAllocationApplication.class, args);
+	}
+
+}
